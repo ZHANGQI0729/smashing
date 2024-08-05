e4f5de8bb6bfe4aa27bd63834938fdb67abf8f50
zhang <3551852183@qq.com>
2024-08-05 10:02:03 +0800

    11

---
+++ src/app/(dashboard)/Collections/page.js
@@ -0,0 +1,7 @@
+import Collections from 'views/Collections/collections-page';
+
+// ==============================|| PAGE ||============================== //
+
+export default function OverviewPagePage() {
+  return <Collections />;
+}
